81a75ebd9c5a58b853552ce9bc58934a1f84120f
krishiv1545 <krishiv1545@gmail.com>
2026-01-31 18:54:54 +0530

    Initial commit

---
+++ src/main/java/com/krishiv1545/clinical_form_gateway/ClinicalFormGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.krishiv1545.clinical_form_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalFormGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalFormGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishiv1545/clinical_form_gateway/ClinicalFormGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishiv1545.clinical_form_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalFormGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
